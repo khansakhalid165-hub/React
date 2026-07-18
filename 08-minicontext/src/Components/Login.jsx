979884cb1a0b8091f1a638633e81e27bd062ef35
Khansa Khalid <EMAIL_USED_ON_GITHUB>
2026-07-18 16:34:28 +0500

    minicontext

---
+++ 08-minicontext/src/Components/Login.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Login(){
+    const [username,setusername]=useState('')
+    const [password,setpassword]=useState('')
+    const { setUser } = useContext(UserContext)
+
+    const handlesubmit=(e)=>{
+        e.preventDefault()
+        setUser({username,password})
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <input
+                type="text"
+                value={username}
+                onChange={(e)=> setusername(e.target.value)}
+                placeholder="username"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e)=> setpassword(e.target.value)}
+                placeholder="password"
+            />
+            <button onClick={handlesubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
